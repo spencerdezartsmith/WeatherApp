510505949d80fdad17be86fcb1693b69f8b31315
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-01-06 11:14:15 -0800

    Repair formatting check

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -22,20 +22,82 @@ class ViewController: UIViewController {
         
         if let unwrappedString = optionalString {
             
-            selectedCity = NSString(string: unwrappedString)
+            let tempString = NSString(string: unwrappedString)
             
             // Check to make sure the string is formatted correctly
             
-            if selectedCity.contains(" ") {
+            if tempString.contains(" ") {
                 
-                selectedCity.replacingOccurrences(of: " ", with: "-")
+                selectedCity = tempString.replacingOccurrences(of: " ", with: "-") as NSString
+                print(selectedCity)
+                
+            } else {
+                
+                selectedCity = tempString as NSString
                 
             }
             
+            // Set up the call to the weather page
             
-            
-            
-            
+            if let url = URL(string: "http://www.weather-forecast.com/locations/\(selectedCity)/forecasts/latest") {
+                
+                let request = NSMutableURLRequest(url: url)
+                
+                let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                    data, response, error in
+                    
+                var forecastData = ""
+                    
+                    if error != nil {
+                        
+                        print("The following error occurred: \(error)")
+                        
+                        self.forecastPlaceholder.text = "Sorry there was no data found for that city. Please try again"
+                        
+                    } else {
+                        
+                        if let unwrappedData = data {
+                            
+                            let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                                
+                            var stringSeperator = "Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">"
+                            
+                            if let contentArray = dataString?.components(separatedBy: stringSeperator) {
+                                
+                                if contentArray.count > 0 {
+                                    
+                                    stringSeperator = "</span>"
+                                    
+                                    let newContentArray = contentArray[1].components(separatedBy: stringSeperator)
+                                    
+                                    if newContentArray.count > 0 {
+                                        
+                                        forecastData = newContentArray[0]
+                                        
+                                        DispatchQueue.main.sync(execute: {
+                                            
+                                            self.forecastPlaceholder.text = forecastData
+                                            
+                                        })
+                                        
+                                    }
+                                    
+                                }
+                                
+                                
+                            }
+                                
+                            
+                        }
+                        
+                    }
+                    
+                    
+                }
+                
+                task.resume()
+                
+            }
         }
         
     }
